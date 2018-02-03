3cdfd6278e53a11d9026a33e177920703305302a
Matthew Mogavero <matt.moga@gmail.com>
2018-02-02 21:13:56 -0500

    edited cursor styling and word selection JS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const paragraph = document.querySelector('#words');
 const wordList = `Tonx fanny pack Tumblr polaroid trust fund art party chambray beard Bushwick Truffaut tote bag flexitarian distillery farm-to-table freegan photo booth letterpress gastropub DIY Williamsburg banh mi Pinterest four loko Kickstarter synth raw denim YOLO master cleanse 8-bit wayfarers brunch Echo Park whatever Blue Bottle street art crucifix VHS viral kale chips dreamcatcher mlkshk lo-fi pork belly Brooklyn messenger bag tattooed bitters bicycle rights sriracha meggings try-hard tofu swag McSweeney's hashtag bespoke sartorial Helvetica fingerstache mustache kitsch keytar Cosby sweater Neutra biodiesel typewriter Pitchfork yr organic jean shorts ethical craft beer next level chia meh plaid cray forage quinoa post-ironic kogi ennui cred scenester pour-over hella chillwave heirloom aesthetic mixtape literally fap vegan readymade vinyl tousled Shoreditch artisan leggings single-origin coffee you probably haven't heard of them Carles skateboard disrupt seitan PBR&B XOXO direct trade  cliche butcher High Life Godard normcore twee food truck Odd Future lomo gluten-free slow-carb flannel Portland paleo pug Austin American Apparel gentrify pop-up actually put a bird on it Thundercats shabby chic mumblecore sustainable banjo ugh Intelligentsia 3 wolf moon roof party occupy Wes Anderson drinking vinegar squid authentic +1 Schlitz small batch asymmetrical deep v locavore stumptown umami semiotics retro hoodie cardigan 90's Marfa fashion axe iPhone church-key cornhole irony keffiyeh selvage Banksy wolf selfies Etsy narwhal salvia PBR fixie before they sold out blog Vice pickled`.split(' ');
 //wraps each string in <span>
-const spans = wordList.map(word => { //map says "for each item..."
+const spans = wordList.map(word => {       //map says "for each item do this (function)""
     const span = document.createElement('span');
     span.textContent = word;
     return span;
@@ -11,14 +11,26 @@ const answer = findLongestWord(wordList);
 let loserAlert; 
 
 //add click event to all the spans that now wrap each individual string
+//also adds spaces between the span elements
 spans.forEach(span => {
+    const spaceSpan = document.createElement('span');
+    spaceSpan.textContent= ' ';
+    paragraph.appendChild(spaceSpan);
     span.addEventListener('click', wordSelect);
+    paragraph.appendChild(span);
 });
+
+//wordselect
+function wordSelect(e) {
+    // console.log(e.target.textContent);
+    const selection = e.target.textContent;
+    console.log(selection);
+}
 
 //Activates timer
 startBtn.addEventListener('click', startGame);
 
- //gaurd to protect mutliple timer sets
+//gaurd to protect mutliple timer sets
  function startGame() {
     if (!loserAlert) {
     loserAlert = window.setTimeout(showAlert, 6000);
@@ -34,6 +46,16 @@ function findLongestWord(stuff) {
         return longestWord[0];
   }
 
+//declares the answer
+function endOfGame() {
+    if (selection === answer) {
+        //stop loser alert
+        alert('You got it!');
+    }
+    selection.addEventListener('click', endOfGame);
+}
+
+
 //inject words into HTML
 
 
@@ -43,6 +65,4 @@ function findLongestWord(stuff) {
   }
 
 // Need to run something to stop lose alert and show winning alert
-//   function clearAlert() {
-//     window.clearTimeout(timeoutID);
-//   }
+//Show how long it took
